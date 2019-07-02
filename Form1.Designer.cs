e436fd1b2235ae44beef5b898bf724a3f301ff1c
WiredUp4Fun <44186300+WiredUp4Fun@users.noreply.github.com>
2019-07-02 10:27:48 +0800

    Push to Github

---
+++ Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace RegexExample
+{
+    partial class FormRegexExample
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormRegexExample));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.buttonFind = new System.Windows.Forms.Button();
+            this.textBoxFind = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.richTextBox1.Font = new System.Drawing.Font("Courier New", 8.25F);
+            this.richTextBox1.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(531, 608);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = resources.GetString("richTextBox1.Text");
+            // 
+            // buttonFind
+            // 
+            this.buttonFind.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonFind.Location = new System.Drawing.Point(585, 38);
+            this.buttonFind.Name = "buttonFind";
+            this.buttonFind.Size = new System.Drawing.Size(75, 23);
+            this.buttonFind.TabIndex = 1;
+            this.buttonFind.Text = "Find";
+            this.buttonFind.UseVisualStyleBackColor = true;
+            this.buttonFind.Click += new System.EventHandler(this.buttonFind_Click);
+            // 
+            // textBoxFind
+            // 
+            this.textBoxFind.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxFind.Location = new System.Drawing.Point(537, 12);
+            this.textBoxFind.Name = "textBoxFind";
+            this.textBoxFind.Size = new System.Drawing.Size(172, 20);
+            this.textBoxFind.TabIndex = 2;
+            this.textBoxFind.Text = "\\d+s";
+            // 
+            // FormRegexExample
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(721, 608);
+            this.Controls.Add(this.textBoxFind);
+            this.Controls.Add(this.buttonFind);
+            this.Controls.Add(this.richTextBox1);
+            this.MaximumSize = new System.Drawing.Size(737, 647);
+            this.MinimumSize = new System.Drawing.Size(737, 647);
+            this.Name = "FormRegexExample";
+            this.Text = "Regex Example";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button buttonFind;
+        private System.Windows.Forms.TextBox textBoxFind;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace RegexExample
+{
+    public partial class FormRegexExample : Form
+    {
+        public FormRegexExample()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonFind_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Regex rgx = new Regex(textBoxFind.Text, RegexOptions.Multiline);
+
+                MatchCollection matches = rgx.Matches(richTextBox1.Text);
+
+                richTextBox1.SelectAll();
+                richTextBox1.SelectionBackColor = Color.White;
+
+                foreach (Match match in matches)
+                {
+                    richTextBox1.Select(match.Index, match.Length);
+                    richTextBox1.SelectionBackColor = Color.LightGreen;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+
+        }
+    }
+}
